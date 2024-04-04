c9e06fc5cd78ebc34bd88089adf52d2e671e3b90
kamau simon <kamausimon217@gmail.com>
2024-04-04 21:42:57 +0300

    created paths in our project

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -1,11 +1,25 @@
 <?php
 
-declare (strict_types=1);
+declare(strict_types=1);
 
 namespace App\Controllers;
 
-class HomeController{
-   public function Home() {
-    echo " Home page";
+use Framework\TemplateEngine;
+//import the paths class here
+use App\Config\Paths;
+
+class HomeController
+{
+   private TemplateEngine $view;
+
+   public function __construct()
+   {
+      $this->view = new TemplateEngine(Paths::class);
+   }
+
+   public function Home()
+   {
+      dd($this->view);
+      echo " Home page";
    }
 }
