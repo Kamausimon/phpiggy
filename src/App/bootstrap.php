ddf2cde7471163895acda84358165bddfd6ccd22
kamau simon <kamausimon217@gmail.com>
2024-04-04 16:13:51 +0300

    controller added

--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 require  __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Controllers\HomeController;
 
 $app = new App();
 
-$app->get('/');
+$app->get('/', [HomeController::class, 'Home']);
 
 
-dd($app);
 
 return $app;
