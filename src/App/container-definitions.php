f99925ae4a7e7b33f86d55a54e86a6f972fed0a9
kamau simon <kamausimon217@gmail.com>
2024-04-21 21:01:00 +0300

    updates

--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
-use Framework\{TemplateEngine, Database};
+use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\ValidatorService;
+use App\Services\{ValidatorService, UserService};
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(Paths::ROOT);
+$dotenv->load();
 
 return [
     TemplateEngine::class => fn () => new TemplateEngine(Paths::VIEW),
@@ -13,5 +17,11 @@ return [
         'host' => $_ENV['DB_HOST'],
         'port' => $_ENV['DB_PORT'],
         'dbname' => $_ENV['DB_NAME']
-    ], $_ENV['DB_USER'], $ENV['DB_PASS'])
+    ], $_ENV['DB_USER'], $_ENV['DB_PASS']),
+
+    UserService::class => function (Container $container) {
+        $db = $container->get(Database::class);
+
+        return new UserService($db);
+    }
 ];
