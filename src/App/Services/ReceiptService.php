e356a304854acb460b757ff31573141e1350a991
kamau simon <kamausimon217@gmail.com>
2024-05-05 01:17:48 +0300

    file upload

---
+++ src/App/Services/ReceiptService.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Framework\Database;
+use Framework\Exceptions\ValidationException;
+
+class ReceiptService
+{
+    public function __construct(private Database $db)
+    {
+    }
+
+    public function validateFile(?array $file)
+    {
+        if (!$file || $file['error'] !== UPLOAD_ERR_OK) {
+            throw new ValidationException([
+                'receipt' => ['failed to upload file']
+            ]);
+        }
+
+        $maxFileSizeMB = 3;
+
+        if ($file['size'] > $maxFileSizeMB) {
+        }
+        dd($file);
+    }
+}
--- src/App/container-definitions.php
+++ src/App/container-definitions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use Framework\{TemplateEngine, Database, Container};
 use App\Config\Paths;
-use App\Services\{ValidatorService, UserService, TransactionService};
+use App\Services\{ValidatorService, UserService, TransactionService, ReceiptService};
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(Paths::ROOT);
@@ -29,5 +29,11 @@ return [
         $db = $container->get(Database::class);
 
         return new TransactionService($db);
+    },
+
+    ReceiptService::class => function (Container $container) {
+        $db = $container->get(Database::class);
+
+        return new ReceiptService($db);
     }
 ];
