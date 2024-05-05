7bbaa62aa9e686290e54df9f61f375629babd17e
kamau simon <kamausimon217@gmail.com>
2024-05-05 19:46:48 +0300

    update

--- src/App/Services/ReceiptService.php
+++ src/App/Services/ReceiptService.php
@@ -6,6 +6,8 @@ namespace App\Services;
 
 use Framework\Database;
 use Framework\Exceptions\ValidationException;
+use App\Config\Paths;
+use Framework\Validator;
 
 class ReceiptService
 {
@@ -42,6 +44,20 @@ class ReceiptService
         if (!in_array($clientMimeType, $allowedMimeTypes)) {
             throw new ValidationException([
                 'receipt' => ['invalid file type']
+            ]);
+        }
+    }
+
+    public function upload(array $file)
+    {
+        $fileExtension = pathinfo($file['name'], PATHINFO_EXTENSION);
+        $newFileName = bin2hex(random_bytes(16)) . "." . $fileExtension;
+
+        $uploadPath = Paths::STORAGE_UPLOADS . "/" . $newFileName;
+
+        if (!move_uploaded_file($file['tmp_name'], $uploadPath)) {
+            throw new ValidationException([
+                'receipt' => ['failed to upload file']
             ]);
         }
     }
