d6fe08f360930ab6b4b87bdcd513723b9745f578
kamau simon <kamausimon217@gmail.com>
2024-05-06 15:05:40 +0300

    update

--- src/App/Services/ReceiptService.php
+++ src/App/Services/ReceiptService.php
@@ -72,4 +72,28 @@ class ReceiptService
             ]
         );
     }
+
+    public function getReceipt(string $id)
+    {
+        $receipt = $this->db->query(
+            "SELECT * FROM receipts WHERE id = :id",
+            [
+                'id' => $id
+            ]
+        )->findAll();
+    }
+
+    public function read(array $receipt)
+    {
+        $filePath = Paths::STORAGE_UPLOADS . '/' . $receipt['storage_filename'];
+
+        if (!file_exists($filePath)) {
+            redirectTo('/');
+        }
+
+        header("Content-Disposition: inline;filename={$receipt['original_filename']}");
+        header("Content-Type: {$receipt['media_type']}");
+
+        readfile($filePath);
+    }
 }
